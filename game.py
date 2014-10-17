34e711c7effb41e67f6adb7d2a9452e6f67d3ad9
Aidan Bracher <aidan.bracher@hotmail.co.uk>
2014-10-17 03:11:39 +0100

    One for submission?

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from map import rooms
 import string
 from PIL import Image
 
+
+
 def remove_punct(text):
     """This function is used to remove all punctuation
     marks from a string. Spaces do not count as punctuation and should
@@ -239,16 +241,15 @@ def credits():
     print("Vasil Grigorov")
 
 def startup():
-    print("!!!!!!You are lost in the reception of the Queens buildings.\nYou have misplaced your wallet, and don't know where.\nThe Robs can give you a map to find where you are.!!!!!!")
+    print("!!!!!!You are lost in the reception of the Queens buildings.\nYou have misplaced your wallet, and don't know where.\nRob can give you a map to find where you are.!!!!!!")
     print("\nCurrently you are in:")
     
 
 def DisplayPic():
     
-    img = Image.open('map.png')
-    img.show()
+    img = Image.open('map.png').show()
 
-
+    
 # This is the entry point of our program
 def main():
     # Start game at the reception
@@ -272,9 +273,8 @@ def main():
         if map == 0 and current_room["name"] == "Robs' room":
             map = 1
             print("!!!!!!You have picked up a map from the robs, your wallet should be in the cafeteria.!!!!!!")
-
-        if map == 1:
             DisplayPic()
+        
 
         if map ==1 and current_room["name"] == "the Cafeteria" and wallet != 1:
             wallet = 1
